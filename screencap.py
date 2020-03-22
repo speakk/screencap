fb8320eed274663c9ec23d20bf290d36d7c1705e
Jesse Viikari <jesse.viikari@vincit.fi>
2020-03-22 22:51:48 +0200

    Neaterish code

--- screencap.py
+++ screencap.py
@@ -14,27 +14,18 @@ import sys
 if len(sys.argv) < 2:
     print("No filename supplied.")
     print("Usage: python screenCap.py [filename].  (omit the extension, .mkv will be added by the script)")
-    exit()
+    sys.exit()
 
-fileName = sys.argv[1]
-  
-# Allow setting the cancel key from environment args, Default: ` 
-cancel_key = ord( 
-    os.environ.get( 
-        'pylogger_cancel', 
-        '`'
-    )[0] 
-) 
+fileName = sys.argv[1] + ".mkv"
 
-fileName = fileName + ".mkv"
 if os.path.isfile(fileName):
     print("File already exists: " + fileName + ".(mp4|mkv)")
     overwrite = input("Do you want to overwrite the file(s)? y/n \n")
     if overwrite == 'n':
-        exit()
+        sys.exit()
     elif overwrite != 'y':
         print("Neither yes or no, exiting until you make up your mind")
-        exit()
+        sys.exit()
 
 print("Ready to capture on: ctrl + f9")
 
@@ -45,17 +36,12 @@ isRCtrlDown = False
 
 capturing = False
 captureProcess = None
-convertProcess = None
 
-# create a hook manager object 
 new_hook = pyxhook.HookManager() 
   
-#creating key pressing event and saving it into log file 
 def OnKeyPress(event): 
     global isLCtrlDown
     global isRCtrlDown
-    global captureCmd
-    global capturing
     global captureProcess
 
     if event.Key == "Control_L":
@@ -66,7 +52,7 @@ def OnKeyPress(event):
 
     if event.Key == 'q':
         global new_hook
-        if capturing:
+        if captureProcess:
             captureProcess.terminate()
             captureProcess.wait()
             new_hook.cancel()
@@ -74,10 +60,9 @@ def OnKeyPress(event):
         exit()
 
     if event.Key == "F9" and (isLCtrlDown or isRCtrlDown):
-        if not capturing:
+        if not captureProcess:
             print("Starting capture!")
             captureProcess = subprocess.Popen(captureCmd, shell=True)
-            capturing = True
         else:
             captureProcess.terminate()
             captureProcess.wait()
@@ -86,7 +71,7 @@ def OnKeyPress(event):
                 exit()
             print("Recording finished, file written: " + fileName)
             print("Ready to capture again with ctrl + f9. Exit with q or ctrl+c.")
-            capturing = False
+            captureProcess = None
 
 def OnKeyUp(event):
     global isLCtrlDown
@@ -99,17 +84,6 @@ def OnKeyUp(event):
 
 new_hook.KeyDown = OnKeyPress 
 new_hook.KeyUp = OnKeyUp 
-# set the hook 
 new_hook.HookKeyboard() 
   
-try: 
-    new_hook.start()         # start the hook 
-except KeyboardInterrupt: 
-    # User cancelled from command line. 
-    pass
-except Exception as ex: 
-    # Write exceptions to the log file, for analysis later. 
-    msg = 'Error while catching events:\n  {}'.format(ex) 
-    pyxhook.print_err(msg) 
-    # with open(log_file, 'a') as f: 
-    #     f.write('\n{}'.format(msg)) 
+new_hook.start()
